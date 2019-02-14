934679900dfa06128ce6ec3fba8b8fc14c4ba782
Vinicius Andrade <vpd.andrade@gmail.com>
2019-02-13 22:00:02 -0200

    commit inicial

---
+++ src/main/java/br/andrade/vpd/ServletInitializer.java
@@ -0,0 +1,14 @@
+package br.andrade.vpd;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PontoInteligenteApplication.class);
+	}
+
+}
+
